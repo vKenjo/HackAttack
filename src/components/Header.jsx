11574506519586276dc0de3a7cb8a1b12592b264
Jam Meisy Tan <jammeisytan@gmail.com>
2024-07-06 05:20:43 +0800

    Header has logo, Waiting for proper SVG

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,13 @@ import {Link} from 'react-router-dom';
 
 const Header = () => {
     return (
-        <header className="w-screen bg-gray-800 text-white min-h-20 flex justify-center sticky ">
-            <div className="h-fit m-auto w-8/12 flex justify-between align-middle">
-                <div className="flex space-x-4">
-                    <h1 className="text-2xl font-bold">Insert Logo Here</h1>
-                    <h1 className="text-2xl">Hi, TechieKid</h1>
-                </div>
+        <header className="w-screen bg-gray-800 text-white h-20 flex justify-center sticky ">
+            <div className="h-auto m-auto w-8/12 flex justify-between align-middle items-center">
+
+                    <img className='h-32' src='/GENERAL/LOGO.svg'/>
+
                 <nav>
-                    <ul className="flex space-x-4">
+                    <ul className="flex space-x-20 text-xl ">
                         <li><Link to="/" className="hover:text-gray-500 ">Home</Link></li>
                         <li><Link to="/singleplayer" className="hover:text-gray-500">Singleplayer</Link></li>
                         <li><Link to="/multiplayer" className="hover:text-gray-500">Multiplayer</Link></li>
