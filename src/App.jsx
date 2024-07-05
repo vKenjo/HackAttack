ed2a8fee92b02b13b46c4abf1423b0ea8c154d5e
Jam Meisy Tan <jammeisytan@gmail.com>
2024-07-05 23:46:09 +0800

    Fixed Redirects and Kenjos

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import Hero from './components/Hero';
 import Footer from './components/Footer';
 import SinglePlayerFrame from './components/Layout/SinglePlayerFrame';
 import LanguageTopics from './components/SP/LanguageTopics';
-import MultiPlayerFrame from "./components/Layout/MultiPlayerFrame.jsx";
+import MultiPlayerFrame from "./components/Layout/MultiPlayerFrame";
+import Learn from './components/SP/topic/Learn'
 
 const Home = () => (
-  <>
-    <Hero />
-    <Footer />
-  </>
+    <div className='flex flex-col h-screen justify-between'>
+        <Header/>
+        <Hero/>
+        <Footer/>
+    </div>
 );
 
 const SinglePlayer = () => (
@@ -23,23 +25,20 @@ const SinglePlayer = () => (
 );
 
 const MultiPlayer = () => (
-  <>
-    <div className="container mx-auto py-20">
-      <h1 className="text-4xl font-bold mb-4">Multiplayer</h1>
-      <p className="text-lg">This is the Multiplayer page.</p>
+    <div className='flex flex-col h-screen justify-between'>
+        <Header/>
+        <MultiPlayerFrame/>
+        <Footer/>
     </div>
-    <HeroMP title={'Multiplayer'} subtitle={'This is the Multiplayer page.'} />
-  </>
 );
 
 const App = () => {
   return (
     <Router>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/SinglePlayer" element={<SinglePlayer />} />
-        <Route path="/Multiplayer" element={<MultiPlayer />} />
+        <Route path="/singleplayer" element={<SinglePlayer />} />
+        <Route path="/multiplayer" element={<MultiPlayer />} />
         <Route path="/language/:id" element={<LanguageTopics />} />
         <Route path="/language/:id/topic/learn" element={<Learn />} />
       </Routes>
