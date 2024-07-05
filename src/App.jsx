d934463a3a50af748cd00aaa27a5dfcc4a263f78
Josh Kenn Viray <joshkennviray@gmail.com>
2024-07-06 00:04:18 +0800

    make as new main

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,41 @@ import Hero from './components/Hero';
 import Footer from './components/Footer';
 import SinglePlayerFrame from './components/Layout/SinglePlayerFrame';
 import LanguageTopics from './components/SP/LanguageTopics';
-import MultiPlayerFrame from "./components/Layout/MultiPlayerFrame";
-import Learn from './components/SP/topic/Learn'
+import Learn from './components/SP/topic/Learn';
+import LearnFrame from './components/Pages/LearnFrame';
 
 const Home = () => (
-    <div className='flex flex-col h-screen justify-between'>
-        <Header/>
-        <Hero/>
-        <Footer/>
-    </div>
+  <>
+    <Hero />
+    <Footer />
+  </>
 );
 
 const SinglePlayer = () => (
-    <div className='flex flex-col h-screen justify-between'>
-        <Header/>
-        <SinglePlayerFrame/>
-        <Footer/>
-    </div>
+  <SinglePlayerFrame />
 );
 
 const MultiPlayer = () => (
-    <div className='flex flex-col h-screen justify-between'>
-        <Header/>
-        <MultiPlayerFrame/>
-        <Footer/>
+  <>
+    <div className="container mx-auto py-20">
+      <h1 className="text-4xl font-bold mb-4">Multiplayer</h1>
+      <p className="text-lg">This is the Multiplayer page.</p>
     </div>
+    <HeroMP title={'Multiplayer'} subtitle={'This is the Multiplayer page.'} />
+  </>
 );
 
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/singleplayer" element={<SinglePlayer />} />
-        <Route path="/multiplayer" element={<MultiPlayer />} />
+        <Route path="/SinglePlayer" element={<SinglePlayer />} />
+        <Route path="/Multiplayer" element={<MultiPlayer />} />
         <Route path="/language/:id" element={<LanguageTopics />} />
         <Route path="/language/:id/topic/learn" element={<Learn />} />
+        <Route path="/language/:id/topic/learn/frame" element={<LearnFrame />} />
       </Routes>
     </Router>
   );
