8cf03be0f51a44e150c71c95cda6603d8c7360db
Jam Meisy Tan <jammeisytan@gmail.com>
2024-07-06 05:35:35 +0800

    Better logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,12 @@ const Header = () => {
     return (
         <header className="w-screen bg-gray-800 text-white h-20 flex justify-center sticky ">
             <div className="h-auto m-auto w-8/12 flex justify-between align-middle items-center">
-
-                    <img className='h-32' src='/GENERAL/LOGO.svg'/>
-
+                    <img className='h-12' src='/GENERAL/LOGO.svg'/>
                 <nav>
                     <ul className="flex space-x-20 text-xl ">
-                        <li><Link to="/" className="hover:text-gray-500 ">Home</Link></li>
-                        <li><Link to="/singleplayer" className="hover:text-gray-500">Singleplayer</Link></li>
-                        <li><Link to="/multiplayer" className="hover:text-gray-500">Multiplayer</Link></li>
+                        <li><Link to="/" className="hover:text-yellow-500 ">Home</Link></li>
+                        <li><Link to="/singleplayer" className="hover:text-yellow-500">Singleplayer</Link></li>
+                        <li><Link to="/multiplayer" className="hover:text-yellow-500">Multiplayer</Link></li>
                     </ul>
                 </nav>
             </div>
